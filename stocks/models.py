5735acbc9c050b949adbf6f3d6bcaa9fdfed985e
hm53 <hm53@pc2-022-l.cs.st-andrews.ac.uk>
2013-11-07 18:53:04 +0000

    Add stocks models to Django project

--- stocks/models.py
+++ stocks/models.py
@@ -1,3 +1,54 @@
 from django.db import models
 
-# Create your models here.
+
+class Exchange(models.Model):
+    '''
+    Represents a stock exchange (e.g. NASDAQ).
+    '''
+    name = models.CharField()
+    abbreviation = models.CharField()
+    reuters_code = models.CharField()
+
+    stocks = models.ManyToManyField(Stock)
+    indexes = models.ManyToManyField(Index)
+
+
+class Index(models.Model):
+    '''
+    Represents an index, a collection of stocks, in an exchange (e.g. FTSE 100).
+    '''
+    name = models.CharField()
+    reuters_code = models.CharField()
+    stocks = models.ManyToManyField(Stock)
+
+
+class Stock(models.Model):
+    '''
+    Represents a stock on a certain stock exchange.
+    '''
+    name = models.CharField()
+    ticker = models.CharField()
+    company = models.ForeignKey(Company, related_name='stocks')
+    exchange = models.ForeignKey(Exchange, related_name='stocks')
+
+
+class Company(models.Model):
+    '''
+    Represents a company. A company may have multiple stocks in multiple exchanges.
+    '''
+    name = models.CharField()
+
+
+class StockPrice(models.Model):
+    '''
+    Represents a stock price (volume, close, etc) on a given trading day.
+    '''
+    stock = models.ForeignKey(Stock, related_name='stock_prices')
+
+    date = models.DateField()
+    volume = models.IntegerField()
+    open = models.DecimalField()
+    close = models.DecimalField()
+    high = models.DecimalField()
+    low = models.DecimalField()
+
