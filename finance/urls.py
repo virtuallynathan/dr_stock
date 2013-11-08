cf8722cd50c3512bc955143ba5a11216d445863e
James Spyt <jamesspyt@hotmail.co.uk>
2013-11-08 19:11:09 +0000

    Updated urls

--- finance/urls.py
+++ finance/urls.py
@@ -1,4 +1,5 @@
-from users.views import login_view, logout_view, logout_confirm, register
+from users.views import login_view, logout_view, logout_confirm
+from users.views import register, profile_page
 from django.conf.urls.defaults import patterns, include, url
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -8,8 +9,9 @@ from django.conf import settings
 urlpatterns = patterns('',
     
     url(r'^account/login$',  login_view),
-    url(r'^account/logout$', logout_view),
+    url(r'^account/logout$', logout_confirm),
     url(r'^account/register$', register),
+    url(r'^account/is_logged_in$', profile_page),
     #other patterns
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
     	{'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
