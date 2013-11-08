9c6bf9ddce710fd32619f45000b01581161eb47c
Hamish Morrison <hm53@st-andrews.ac.uk>
2013-11-08 02:19:11 +0000

    Update database setup script so it's OK to run multiple times

--- setup_db.py
+++ setup_db.py
@@ -1,13 +1,17 @@
 from stocks.models import Exchange, Index
 from data.yahoo.index import scrape
 
-lse = Exchange(name='London Stock Exchange', abbreviation='LSE', reuters_code='L')
+lse, created = Exchange.objects.get_or_create(abbreviation='LSE',
+    defaults={'name': 'London Stock Exchange',
+              'reuters_code': 'L'})
 lse.save()
 
-ftse100 = Index(name='FTSE 100', exchange=lse, reuters_code='FTSE')
+ftse100, created = Index.objects.get_or_create(reuters_code='FTSE',
+    defaults={'name': 'FTSE 100',
+              'exchange': lse})
 ftse100.save()
 
 
 stocks = scrape(ftse100)
-print stocks
+stocks.save()
 
