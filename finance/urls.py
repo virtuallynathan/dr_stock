e0b343a61d65e99fef8db29f30b76969fa009f6b
Hamish Morrison <hm53@st-andrews.ac.uk>
2013-11-09 18:37:43 +0000

    Update to Django 1.6, add templates and use default views for user auth

--- finance/urls.py
+++ finance/urls.py
@@ -1,18 +1,20 @@
-from users.views import login_view, logout_view, logout_confirm
-from users.views import register, profile_page
-from django.conf.urls.defaults import patterns, include, url
-from django.conf.urls.static import static
+from django.conf.urls import patterns, include, url
+
 from django.contrib import admin
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.conf import settings
+admin.autodiscover()
+
+from django.contrib.auth.views import login, logout
+
+from users.views import profile
 
 urlpatterns = patterns('',
-    
-    url(r'^account/login$',  login_view),
-    url(r'^account/logout$', logout_confirm),
-    url(r'^account/register$', register),
-    url(r'^account/is_logged_in$', profile_page),
-    #other patterns
-    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
-    	{'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+    # Examples:
+    # url(r'^$', 'finance.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^accounts/login/$',  login, {'template_name': 'login.html'}),
+    url(r'^accounts/logout/$', logout, {'template_name': 'logout.html'}),
+    url(r'^accounts/profile/$', profile, {'template_name': 'profile.html'}),
 )
--- users/views.py
+++ users/views.py
@@ -1,55 +1,19 @@
-from django.template import loader, RequestContext
-from django.template import Context
-from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
-from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, logout
 from django import forms
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
 
-# Login in a user and check if they have registered
-def login_view(request):
-    username = request.POST['username']
-    password = request.POST['password']
-    user = authenticate(username=username, password=password)
-    if request.user.is_authenticated():
-    	if user.is_active:
-    		print "You provided a correct username and password!"
-    		return HttpResponseRedirect("account/is_logged_in")
-    	else:
-    		print "Your account has been disabled"
-    else:
-    	print "Your username and password were incorrect."
-    	return HttpResponseRedirect("account/invalid")
-    return render(request, 'login.html', locals(), context_instance = RequestContext(request))
-  
-# Log a user out
-def logout_view(request):
-    logout(request)
-    return HttpResponseRedirect("/account/logout")
-
-# Confirm a user has logged out
-def logout_confirm(request):
-	return render(request, 'logout.html')
-
-# Allow an investor to register an account
-# Does not add to database
 def register(request):
     if request.method == 'POST':
-        form = UserCreatForm(request.POST)
+        form = UserCreationForm(request.POST)
         if form.is_valid():
-            username = form.clean_username()
-            password = form.clean_password()
             new_user = form.save()
-            user = authenticate(username=username, password =password)
-            login(request, user)
-            return HttpResponseRedirect("/acount/is_logged_in")
+            return HttpResponseRedirect('/')
     else:
         form = UserCreationForm()
-    return render(request, "registration.html", {
-    	'form': form,
-    })
+    return render(request, 'register.html', {'form': form})
 
-# Link to profile page
-def profile_page(request):
-	return render(Request, "profile.html")
+@login_required
+def profile(request):
+    return render(request, 'profile.html')
