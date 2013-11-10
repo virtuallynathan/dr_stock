b0f6ada03139cf5d674f119adae7603527a67ef7
Hamish Morrison <hm53@st-andrews.ac.uk>
2013-11-10 00:33:41 +0000

    Clear out everything in the database when the setup_db script is run

--- setup_db.py
+++ setup_db.py
@@ -1,12 +1,23 @@
-from stocks.models import Exchange, Index
+from django.db import connection
+
+from stocks.models import Exchange, Symbol
 from data.yahoo.index import scrape
+
+
+cursor = connection.cursor()
+try:
+    cursor.execute('drop schema public cascade;')
+    cursor.execute('create schema public;')
+finally:
+    cursor.close()
+
 
 lse, created = Exchange.objects.get_or_create(abbreviation='LSE',
     defaults={'name': 'London Stock Exchange',
               'reuters_code': 'L'})
 lse.save()
 
-ftse100, created = Index.objects.get_or_create(reuters_code='FTSE',
+ftse100, created = Symbol.objects.get_or_create(ticker='FTSE',
     defaults={'name': 'FTSE 100',
               'exchange': lse})
 ftse100.save()
