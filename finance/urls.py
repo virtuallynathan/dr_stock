02232c49e9cd20a9eacafecd15f8ed2d4eb67557
Hamish Morrison <hm53@st-andrews.ac.uk>
2013-11-17 00:02:42 +0000

    Add a view for viewing an index

--- finance/urls.py
+++ finance/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
-admin.autodiscover()
-
 from django.contrib.auth.views import login, logout
 
+from stocks.views import view_index
 from users.views import profile, register
+
+
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
@@ -18,4 +19,6 @@ urlpatterns = patterns('',
     url(r'^accounts/logout/$', logout, {'template_name': 'logout.html'}),
     url(r'^accounts/profile/$', profile, {'template_name': 'profile.html'}),
     url(r'^accounts/register/$', register, {'template_name': 'register.html'}),
+
+    url(r'^index/(?P<ticker>\w+)/$', view_index, {'template_name': 'view_index.html'})
 )
--- stocks/views.py
+++ stocks/views.py
@@ -1,1 +1,35 @@
-# Create your views here.
+import datetime
+from django.shortcuts import render
+
+from data.yahoo.index import scrape_index
+from data.yahoo.price import scrape_price
+
+from stocks.models import get_symbol, get_exchange, get_company, Symbol, Price
+
+
+def view_index(request, ticker, template_name='view_index.html'):
+	try:
+		index = Symbol.objects.get(type=Symbol.INDEX, ticker=ticker)
+	except Symbol.DoesNotExist:
+		result = {'error': 'That index does not exist!'}
+	else:
+		# Scrape components if more than a day old
+		now = datetime.datetime.utcnow()
+		if index.updated.date() < now.date():
+			index = scrape_index(index)
+
+		try:
+			price = index.price
+		except Price.DoesNotExist:
+			price = scrape_price(index)
+			index.price = price
+			price.save()
+			index.save()
+		else:
+			if (now - index.price.updated.replace(tzinfo=None)).total_seconds() > 15 * 60:
+				price = scrape_price(index)
+				price.save()
+
+		result = {'index': index}
+
+	return render(request, template_name, result)
