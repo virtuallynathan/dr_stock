ce8ac643f00f5a75a7f0505fae57f1f89a72b698
root <root@jhproj05.cs.st-andrews.ac.uk>
2013-11-19 16:28:58 +0000

    Set up polling for getting json

--- finance/static/js/controllers.js
+++ finance/static/js/controllers.js
@@ -1,7 +1,15 @@
 var stockApp = angular.module('stockApp', []);
 
-stockApp.controller('StockListCtrl', function ($scope, $http) {
-  $http.get('/data/index/FTSE/').success(function(data) {
-    $scope.stocks = data;
-  });
+stockApp.controller('StockListCtrl', function($scope, $http, $timeout) {
+  (function refresh() {
+    $http.get('/data/index/FTSE/').success(
+      function(data) {
+        $scope.stocks = data;
+        $timeout(refresh, 1000 * 60 * 5);
+      }).error(
+        function(error) {
+          $scope.stocks = ['everythings fucked'];
+          $timeout(refresh, 1000 * 60 * 5);
+        }
+    );})();
 });
