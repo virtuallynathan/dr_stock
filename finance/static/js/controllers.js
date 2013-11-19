96a34b410c855fd0041304204e7ff8a4ba57f751
root <root@jhproj05.cs.st-andrews.ac.uk>
2013-11-19 15:22:41 +0000

    something hamish changed

--- finance/static/js/controllers.js
+++ finance/static/js/controllers.js
@@ -1,7 +1,7 @@
 var stockApp = angular.module('stockApp', []);
 
 stockApp.controller('StockListCtrl', function ($scope, $http) {
-  $http.get('data/index/FTSE/').success(function(data) {
+  $http.get('/data/index/FTSE/').success(function(data) {
     $scope.stocks = data;
   });
 });
