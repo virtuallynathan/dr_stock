82af7968321e9119323a41224abd1c71f7788819
Nathan Owens <virtuallynathan@evoboxes.org>
2013-11-19 13:31:10 +0000

    add some stuff for viewing the index

---
+++ finance/views.py
@@ -0,0 +1,10 @@
+from django import forms
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+
+def view_index();
+	return render(request, 'showIndexes.html')
+	
