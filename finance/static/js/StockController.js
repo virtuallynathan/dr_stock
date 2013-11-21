bb16ca8477bcf556b909e9492e184ab548cdbdba
Hamish Morrison <hm53@st-andrews.ac.uk>
2013-11-21 21:19:06 +0000

    Stocks to test on server

--- finance/static/js/StockController.js
+++ finance/static/js/StockController.js
@@ -13,3 +13,22 @@ StockApp.controller('StockListCtrl', function($scope, $http, $timeout) {
         }
     );})();
 });
+
+StockApp.controller('HistoricalCtrl', function($scope, $http, $timeout) {
+  function processData(data) {
+    for (index in data) {
+      datum = data[index];
+      datum.date = parseDate(datum.date)
+    }
+  }
+  (function refresh() {
+    $http.get('/data/historical/' + exchange + '/' + ticker + '/2013-10-21/2013-11-21/').success(
+      function(data) {
+        $scope.historical = processData(data);
+      }).error(
+        function(error) {
+          $scope.stocks = ['everythings fucked'];
+          $timeout(refresh, 1000 * 60 * 5);
+        }
+    );})();
+});
