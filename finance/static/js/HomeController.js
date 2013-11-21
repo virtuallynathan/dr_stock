bfc2ef90ce1ddcc2a6fc1cf64035a6bef61fded7
Hamish Morrison <hm53@st-andrews.ac.uk>
2013-11-21 13:33:20 +0000

    Add biggest fallers and biggest by market cap

--- finance/static/js/HomeController.js
+++ finance/static/js/HomeController.js
@@ -2,13 +2,32 @@ var homeApp = angular.module('homeApp', []);
 
 homeApp.controller('homeCtrl', function($scope, $http, $timeout) {
   (function refresh() {
-    $http.get('/data/biggest/10/').success(
+    $http.get('/data/risers/3/').success(
       function(data) {
-        $scope.stocks = data;
+        $scope.risers = data;
         $timeout(refresh, 1000 * 60 * 5);
       }).error(
         function(error) {
-          $scope.stocks = ['everythings fucked'];
+          $timeout(refresh, 1000 * 60 * 5);
+        }
+    );})();
+  (function refresh() {
+    $http.get('/data/fallers/3/').success(
+      function(data) {
+        $scope.fallers = data;
+        $timeout(refresh, 1000 * 60 * 5);
+      }).error(
+        function(error) {
+          $timeout(refresh, 1000 * 60 * 5);
+        }
+    );})();
+  (function refresh() {
+    $http.get('/data/biggest/3/').success(
+      function(data) {
+        $scope.biggest = data;
+        $timeout(refresh, 1000 * 60 * 5);
+      }).error(
+        function(error) {
           $timeout(refresh, 1000 * 60 * 5);
         }
     );})();
