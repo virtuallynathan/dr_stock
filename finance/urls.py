863158f045795f4b8206f8a95b82abe89025a688
Hamish Morrison <hm53@st-andrews.ac.uk>
2013-11-21 16:58:03 +0000

    Fix login page to not show if logged in

--- finance/urls.py
+++ finance/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from django.contrib.auth.views import login, logout
 
 import data.urls
-from users.views import profile, register, send_email, sent
-from finance.views import view_index, view_home, view_stock 
+from users.views import profile, register, send_email, sent, login, logout
+from finance.views import view_index, view_home, view_stock
 from finance.views import view_historical_data
 
 
@@ -24,11 +23,11 @@ urlpatterns = patterns('',
     url(r'^view_stock/', view_stock),
     url(r'^view_historical_data/', view_historical_data),
 
-    url(r'^accounts/login/$',  login, {'template_name': 'login.html'}),
+    url(r'^accounts/login/$', login, {'template_name': 'login.html'}),
     url(r'^accounts/logout/$', logout, {'template_name': 'logout.html'}),
     url(r'^accounts/profile/$', profile, {'template_name': 'profile.html'}),
     url(r'^accounts/register/$', register, {'template_name': 'register.html'}),
-    url(r'^$', view_home), 
+    url(r'^$', view_home),
 
     # Messing around with sending email
     url(r'^accounts/email/$',  send_email),
--- users/views.py
+++ users/views.py
@@ -1,11 +1,14 @@
 from django import forms
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.core.mail import EmailMessage
 
+
+@csrf_protect
+@never_cache
 def register(request, template_name='register.html'):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
@@ -19,9 +22,11 @@ def register(request, template_name='register.html'):
         form = UserCreationForm()
     return render(request, template_name, {'form': form})
 
+
 @login_required
 def profile(request, template_name='home.html'):
     return render(request, template_name)
+
 
 # Send an email from form
 def send_email(request):
@@ -45,3 +50,50 @@ def send_email(request):
 
 def sent(request, template_name='sent.html'):
     return render(request, template_name)
+
+
+@csrf_protect
+@never_cache
+def login(request, template_name='users/login.html'):
+    redirect_to = request.POST.get(redirect_field_name,
+                                   request.GET.get(redirect_field_name, ''))
+
+    if not is_safe_url(url=redirect_to, host=request.get_host()):
+        redirect_to = resolve_url(settings.LOGIN_REDIRECT_URL)
+
+    if user.is_authenticated():
+        return HttpResponseRedirect(redirect_to)
+
+    if request.method == "POST":
+        form = authentication_form(request, data=request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return HttpResponseRedirect(redirect_to)
+    else:
+        form = authentication_form(request)
+
+    current_site = get_current_site(request)
+
+    context = {
+        'form': form,
+        'next': redirect_to
+    }
+
+    return render(request, template_name, context)
+
+
+def logout(request, template_name='users/logout.html'):
+    auth_logout(request)
+
+    redirect_to = request.POST.get(redirect_field_name,
+                                   request.GET.get(redirect_field_name, ''))
+
+    if not is_safe_url(url=redirect_to, host=request.get_host()):
+        redirect_to = resolve_url(settings.LOGIN_REDIRECT_URL)
+
+    if redirect_to:
+        # Redirect to this page until the session has been cleared.
+        return HttpResponseRedirect(next_page)
+
+    return HttpResponseReidrect('/')
+
