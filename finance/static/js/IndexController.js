5d3b5527fd24e19eb3b17c51dca9fe8c6a884a1c
Robert Dixon <hrd2@st-andrews.ac.uk>
2013-11-21 23:27:25 +0000

    Fixy Fixy

--- finance/static/js/IndexController.js
+++ finance/static/js/IndexController.js
@@ -2,7 +2,7 @@ var IndexApp = angular.module('IndexApp', []);
 
 IndexApp.controller('IndexListCtrl', function($scope, $http, $timeout) {
 
-  $scope.getBarHeight = function(stock, circ_id) {
+  $scope.getBarHeight = function(stocks, circ_id) {
     var val = ((stocks.price.price / stocks.price.last_close - 1) * 100).toFixed(3);
 
     if (val > 0) {
