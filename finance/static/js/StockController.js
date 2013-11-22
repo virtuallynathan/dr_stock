7d17834edb46987a1b6ed4ca715cbe8a5351304d
Hamish Morrison <hm53@st-andrews.ac.uk>
2013-11-22 20:03:40 +0000

    Improve extensibility of stock-chart directive

--- finance/static/js/StockController.js
+++ finance/static/js/StockController.js
@@ -13,65 +13,60 @@ StockApp.controller('StockListCtrl', function($scope, $http, $timeout) {
         }
     );})();
 });
-/*
-StockApp.controller('HistoricalCtrl', function($scope, $http, $timeout) {
-  function processData(data) {
-    for (index in data) {
-      datum = data[index];
-      datum.date = parseDate(datum.date)
-    }
-  }
-  (function refresh() {
-    $http.get('/data/historical/' + exchange + '/' + ticker + '/2013-10-21/2013-11-21/').success(
-      function(data) {
-        $scope.historical = processData(data);
-      }).error(
-        function(error) {
-          $scope.stocks = ['everythings fucked'];
-          $timeout(refresh, 1000 * 60 * 5);
-        }
-    );})();
-});
-*/
+
+
 StockApp.directive('stockChart', function ($parse) {
     var directiveDefinitionObject = {
         restrict: 'E',
         replace: true,
-        scope: {exchange: '@exchange', ticker: '@ticker'},
+        scope: {exchange: '@exchange', ticker: '@ticker',
+                startDate: '@startDate', endDate: '@endDate'},
         template: '<div class="chart"></div>',
         controller: function ($scope, $http) {
-          function processData(data) {
+          $scope.formatDate = d3.time.format("%Y-%m-%d");
+          $scope.parseDate = $scope.formatDate.parse;
+
+          $scope.processData = function(data) {
             for (index in data) {
-              var datum = data[index];
-              datum.date = $scope.parseDate(datum.date);
+              data[index].date = $scope.parseDate(data[index].date);
             }
             return data;
           }
 
-          $http.get('/data/historical/' + $scope.exchange + '/' + $scope.ticker + '/2012-01-21/2012-05-21/')
-            .success(function(data) {
-              console.log($scope.historical);
-              console.log('going to set');
-              $scope.historical = processData(data.historical);
-              $scope.x.domain(d3.extent($scope.historical, function(d) { return d.date; }));
-              $scope.y.domain(d3.extent($scope.historical, function(d) { return d.close; }));
+          $scope.updateChart = function(data) {
+            $scope.x.domain(d3.extent(data, function(d) { return d.date; }));
+            $scope.y.domain(d3.extent(data, function(d) { return d.close; }));
+            $scope.y.nice();
 
-              $scope.svg.select(".x.axis").call($scope.xAxis);
-              $scope.svg.select(".y.axis").call($scope.yAxis);
+            $scope.svg.select(".x.axis").call($scope.xAxis);
+            $scope.svg.select(".y.axis").call($scope.yAxis);
+            $scope.svg.selectAll("path.line").data([data]).attr("d", $scope.line);
+          }
 
-              $scope.svg.selectAll("path.line").data([$scope.historical]).attr("d", $scope.line);
-              console.log($scope.historical);
-            }).error(function(error) {
-              console.log('you gone and fucked up again aintcha');
-            });
+          $scope.fetchData = function(exchange, ticker, startDate, endDate) {
+            var startDateStr = $scope.formatDate(startDate);
+            var endDateStr = $scope.formatDate(endDate);
+            $http.get('/data/historical/' + exchange + '/' + ticker + '/' + startDateStr + '/' + endDateStr + '/')
+              .success(function(data) {
+                console.log($scope.historical);
+                console.log('going to set');
+                $scope.historical = $scope.processData(data.historical);
+                console.log($scope.historical);
+                $scope.updateChart($scope.historical);
+              }).error(function(error) {
+                console.log('you gone and fucked up again aintcha');
+              });
+          }
+
+          var startDate = $scope.parseDate($scope.startDate);
+          var endDate = $scope.parseDate($scope.endDate);
+          $scope.fetchData($scope.exchange, $scope.ticker, startDate, endDate);
         },
         link: function (scope, element, attrs) {
-          scope.historical = [];
-          scope.parseDate = d3.time.format("%Y-%m-%d").parse;
-          
-var margin = {top: 20, right: 20, bottom: 30, left: 50},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+          var margin = {top: 20, right: 20, bottom: 30, left: 50},
+              width = 960 - margin.left - margin.right,
+              height = 500 - margin.top - margin.bottom;
+
           scope.x = d3.time.scale().range([0, width]);
           scope.y = d3.scale.linear().range([height, 0]);
 
@@ -88,9 +83,6 @@ var margin = {top: 20, right: 20, bottom: 30, left: 50},
             .append("g")
               .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-          scope.x.domain(d3.extent(scope.historical, function(d) { return d.date; }));
-          scope.y.domain(d3.extent(scope.historical, function(d) { return d.close; }));
-
           scope.svg.append("g")
               .attr("class", "x axis")
               .attr("transform", "translate(0," + height + ")")
@@ -104,15 +96,9 @@ var margin = {top: 20, right: 20, bottom: 30, left: 50},
               .style("text-anchor", "end")
               .text("Price");
 
-          scope.svg.append("path")
-              .data([scope.historical])
-              .attr("class", "line")
-              .attr("d", scope.line)
+          scope.svg.append("path").attr("class", "line");
         }
     };
     return directiveDefinitionObject;
 });
 
-function Ctrl($scope) {
-    $scope.data = [10,20,30,40,60, 80, 20, 50];
-}
