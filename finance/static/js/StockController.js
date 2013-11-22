5f1f7f5dc913c20b58ef1a5cf746e4f73b560cfe
Hamish Morrison <hm53@st-andrews.ac.uk>
2013-11-22 00:46:31 +0000

    Make the graph work WOOOOOOOOOOOOOOOOOOOOOOO

--- finance/static/js/StockController.js
+++ finance/static/js/StockController.js
@@ -39,23 +39,31 @@ StockApp.directive('stockChart', function ($parse) {
         replace: true,
         scope: {},
         template: '<div class="chart"></div>',
-        /*controller: function (scope) {
+        controller: function c ($scope, $http) {
           function processData(data) {
             for (index in data) {
-              datum = data[index];
-              datum.date = parseDate(datum.date)
+              var datum = data[index];
+              datum.date = $scope.parseDate(datum.date);
             }
+            return data;
           }
 
-          $http.get('/data/historical/' + exchange + '/' + ticker + '/2013-10-21/2013-11-21/')
+          $http.get('/data/historical/' + exchange + '/' + ticker + '/2012-01-21/2012-05-21/')
             .success(function(data) {
-              scope.historical = processData(data);
+              console.log($scope.historical);
+              console.log('going to set');
+              $scope.historical = processData(data.historical);
+              //$scope.historical = processData([{date: '2012-01-01', close: 100}, {date: '2012-02-01', close:200}])
+              $scope.x.domain(d3.extent($scope.historical, function(d) { return d.date; }));
+              $scope.y.domain(d3.extent($scope.historical, function(d) { return d.close; }));
+              $scope.svg.selectAll("path.line").data([$scope.historical]).attr("d", $scope.line);
+              console.log($scope.historical);
             }).error(function(error) {
               console.log('you gone and fucked up again aintcha');
             });
-        }*/
+        },
         link: function (scope, element, attrs) {
-          scope.historical = [{date: '2012-01-01', close: 100}, {date: '2012-02-01', close:200}]
+          scope.historical = [];//[{date: '2012-01-01', close: 100}, {date: '2012-02-01', close:200}]
           scope.parseDate = d3.time.format("%Y-%m-%d").parse;
           for (index in scope.historical) { datum = scope.historical[index]; datum.date = scope.parseDate(datum.date); }
           
@@ -69,15 +77,15 @@ var margin = {top: 20, right: 20, bottom: 30, left: 50},
           var yAxis = d3.svg.axis().scale(scope.y).orient("left");
 
           var line = d3.svg.line()
-              .x(function(d) { return x(d.date); })
-              .y(function(d) { return y(d.close); });
-
+              .x(function(d) { return scope.x(d.date); })
+              .y(function(d) { return scope.y(d.close); });
+          scope.line = line;
           var svg = d3.select(element[0]).append("svg")
               .attr("width", width + margin.left + margin.right)
               .attr("height", height + margin.top + margin.bottom)
             .append("g")
               .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
+          scope.svg = svg;
           scope.x.domain(d3.extent(scope.historical, function(d) { return d.date; }));
           scope.y.domain(d3.extent(scope.historical, function(d) { return d.close; }));
 
@@ -95,11 +103,11 @@ var margin = {top: 20, right: 20, bottom: 30, left: 50},
               .attr("dy", ".71em")
               .style("text-anchor", "end")
               .text("Price");
-
+          //console.log(line);
           svg.append("path")
-              .data(scope.historical)
+              .data([scope.historical])
               .attr("class", "line")
-              .attr("d", line);
+              .attr("d", line)
         }
     };
     return directiveDefinitionObject;
