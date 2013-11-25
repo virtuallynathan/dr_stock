3429c0f7334555f3a6e2042e3e1692dfc0af4586
Robert Dixon <hrd2@st-andrews.ac.uk>
2013-11-25 14:00:06 +0000

    I don't have a CLUE what I'm doing!

--- finance/views.py
+++ finance/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from data.models import quotes
 
 def view_home(request):
     return render(request, 'home.html')
@@ -12,7 +13,9 @@ def view_stock(request):
     return render(request, 'stocks.html')
 
 def view_recommendation(request):
-    return render(request, 'recommendStock.html')
+
+
+    return json_response({'Hello':'World'});
 
 def view_portfolio(request):
     return render(request, 'portfolio.html')
