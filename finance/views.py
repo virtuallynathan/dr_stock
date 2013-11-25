399feb55edd973a1bd6499a9524824e914b4fe03
Robert Dixon <hrd2@st-andrews.ac.uk>
2013-11-25 14:02:52 +0000

    JSON?

--- finance/views.py
+++ finance/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from data.models import Quote
+from data.views import serialize_symbol, json_response
 
 def view_home(request):
     return render(request, 'home.html')
