0cf9cbc67e8005897f32a7ec24dd71b62e0724b8
Hamish Morrison <hm53@st-andrews.ac.uk>
2013-11-26 00:56:18 +0000

    Rank recommends etc

--- finance/static/js/recommend.js
+++ finance/static/js/recommend.js
@@ -5,6 +5,9 @@ RecommendApp.controller('RecommendCtrl', function($scope, $http, $timeout) {
   var getData = function () {
     $http.get('/recommendations/10/')
       .success(function(data) {
+        for (var i = 0; i < data.length; i++) {
+          data.rank = i;
+        }
         $scope.recommended = data;
         $timeout(getData, 1000 * 60 * 60);
       }).error(function(error) {
